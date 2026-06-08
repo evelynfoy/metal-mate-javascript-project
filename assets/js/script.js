217357696318590b8a11b9feb71803ceae763564
Evelyn Foy <evelyn.foy@hotmail.com>
2026-06-08 08:31:30 +0100

    Added preferences area, styling and functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,72 @@
 "use strict";
 /*jshint strict: global */
 /*jshint esversion: 8 */
+
+// Define references to html document elements
+const motivationalAreaRef = document.querySelector('#motivational-area');
+const metalChoiceRef = document.querySelector('#metal-choice');
+const currencyChoiceRef = document.querySelector('#currency-choice');
+/**
+ * If mode is metals then it returns a list of valid symbols otherwise it returns the specific price requested.
+ * For symbols it fetches the list using the https://api.gold-api.com/symbols url
+ * For price information it retrieves the preferences made by the user and uses 
+ * the https://api.gold-api.com/price/${metal}/${currency} url 
+ * If the site is unavailable then an alert message is shown and the button is disabled.
+ * @param {string} mode 
+ * @returns {Response} Results of the fetch requested, categories or questions
+ */
+async function fetchDataFromAPI(mode) {
+
+  const metal = metalChoiceRef.value;
+  const currency = currencyChoiceRef.value;
+
+  const url = `https://api.gold-api.com/symbols`;
+
+  try {
+    const result = await fetch(url);
+    return await result.json();
+  } 
+  catch (error) {
+    alert(
+      `Unfortunately the prices site is currently unavailable. 
+Please try again later.`);
+    buttonRef.disabled = true;
+  }
+}
+
+/**
+ * Calls the async function that fetches the symbols using an API passing in a mode of 'metals'.
+ * Creates an html option for each symbol and updates the html page with the list
+ */
+async function loadMetals() {
+
+  const metals = await fetchDataFromAPI("metals");
+  let html = `<select name="Gold" value="XAU" id="metal-choice" >`;
+
+  metals.forEach(
+    metal => {
+      html += `<option value="${metal.symbol}">${metal.name}</option>`;
+    }
+  );
+
+  metalChoiceRef.innerHTML = html;
+}
+
+/**
+ * Creates an html option for each currency in a currency array and updates the html page with the list
+ */
+function loadCurrencies() {
+
+  let currencies = ["EUR", "AUD", "BRL", "CAD", "CHF", "CNY", "DKK",  "GBP", "HKD", "INR","JPY", "KRW", "MXN", "NOK", "NZD", "SEK", "SGD", "USD", "ZAR"];
+  let html = `<select name="USD" value="USD" id="currency-choice" >`;
+  currencies.forEach(
+    currency => {
+      html += `<option value="${currency}">${currency}</option>`;
+    }
+  );
+  currencyChoiceRef.innerHTML = html;
+}
+
+// Load metals options from API 
+loadMetals();
+loadCurrencies();
